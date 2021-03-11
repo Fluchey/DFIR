181bd6aad5c55145f188be8220e9b2fa193bd4cb
Anton <anton.fluch@gmail.com>
2021-03-11 19:52:24 +0100

    Update Collect_logs.ps1

--- Evidence_collection/Collect_logs.ps1
+++ Evidence_collection/Collect_logs.ps1
@@ -17,39 +17,44 @@ Write-Output "----- SCRIPT STARTED -----"
         New-Item -Path $ComputerDesktopPath"\" -Name "RanAsAdmin.txt" -ItemType "file" -Value "$IsAdmin" | Out-Null
 
     #Information about machine
-        (Get-WMIObject win32_operatingsystem).name | Out-File -FilePath $ComputerDesktopPath"\OS_name.txt"
-        [environment]::OSVersion.Version | Out-File -FilePath $ComputerDesktopPath"\OS_version.txt"
+        (Get-WMIObject win32_operatingsystem).name | Out-File -FilePath $ComputerDesktopPath"\OS_name_$ComputerName.txt"
+        [environment]::OSVersion.Version | Out-File -FilePath $ComputerDesktopPath"\OS_version_$ComputerName.txt"
     
     #Wineventlogs
         Write-Output "----- Collecting WinEventLogs -----"
         mkdir $ComputerDesktopPath"\Wineventlog" | Out-Null
-        Get-EventLog -LogName Application > $ComputerDesktopPath"\Wineventlog\"$ComputerName"_Applicationlog.evtx"
-        Get-EventLog -LogName System > $ComputerDesktopPath"\Wineventlog\"$ComputerName"_Systemlog.evtx"
-        if ($IsAdmin) {
-            Get-EventLog -LogName Security > $ComputerDesktopPath"\Wineventlog\"$ComputerName"_Securitylog.evtx"
+        if ($IsAdmin){
+            Copy-Item "C:\Windows\System32\winevt\Logs\Application.evtx" -Destination "$ComputerDesktopPath\Wineventlog\ApplicationLog_$ComputerName.evtx"
+            Copy-Item "C:\Windows\System32\winevt\Logs\System.evtx" -Destination "$ComputerDesktopPath\Wineventlog\SystemLog_$ComputerName.evtx"
+            Copy-Item "C:\Windows\System32\winevt\Logs\Security.evtx" -Destination "$ComputerDesktopPath\Wineventlog\SecurityLog_$ComputerName.evtx"
+            }else {
+                Get-EventLog -LogName Application > $ComputerDesktopPath"\Wineventlog\"$ComputerName"_Applicationlog.evtx"
+                Get-EventLog -LogName System > $ComputerDesktopPath"\Wineventlog\"$ComputerName"_Systemlog.evtx"
             }
 
     #Registry extraction
         Write-Output "----- Collecting Registry information -----"
         mkdir $ComputerDesktopPath"\Registry" | Out-Null
-        reg export HKLM\Software $ComputerDesktopPath"\Registry\SOFTWARE.reg"
-        reg export HKLM\System $ComputerDesktopPath"\Registry\SYSTEM.reg"
+        reg export HKLM\Software $ComputerDesktopPath"\Registry\SOFTWARE_$ComputerName.reg" | Out-Null
+        reg export HKLM\System $ComputerDesktopPath"\Registry\SYSTEM_$ComputerName.reg" | Out-Null
     
     #Get running processes
         Write-Output "----- Collecting Running processes -----"
         mkdir $ComputerDesktopPath"\Running_processes" | Out-Null
-        tasklist | Out-File -FilePath $ComputerDesktopPath"\Running_processes\tasklist.txt"
-        tasklist /v | Out-File -FilePath $ComputerDesktopPath"\Running_processes\tasklist_with_runtime.txt"
-        tasklist /m | Out-File -FilePath $ComputerDesktopPath"\Running_processes\tasklist_with_dlls.txt"
-        Net start | Out-File -FilePath $ComputerDesktopPath"\Running_processes\started_services.txt"
-        Schtasks | Out-File -FilePath $ComputerDesktopPath"\Running_processes\scheduled_tasks.txt"
+        tasklist | Out-File -FilePath $ComputerDesktopPath"\Running_processes\tasklist_$ComputerName.txt"
+        tasklist /v | Out-File -FilePath $ComputerDesktopPath"\Running_processes\tasklist_with_runtime_$ComputerName.txt"
+        tasklist /m | Out-File -FilePath $ComputerDesktopPath"\Running_processes\tasklist_with_dlls_$ComputerName.txt"
+        Net start | Out-File -FilePath $ComputerDesktopPath"\Running_processes\started_services_$ComputerName.txt"
+        Schtasks | Out-File -FilePath $ComputerDesktopPath"\Running_processes\scheduled_tasks_$ComputerName.txt"
 
     #Get network information
         Write-Output "----- Collecting Network information -----"
         mkdir $ComputerDesktopPath"\Network" | Out-Null
-        netstat -nbao | Out-File -FilePath $ComputerDesktopPath"\Network\netstat_active_connections.txt"
-        ipconfig /displaydns | Out-File -FilePath $ComputerDesktopPath"\Network\ipconfig-displaydns.txt"
-        ipconfig /all | Out-File -FilePath $ComputerDesktopPath"\Network\ipconfig-all.txt"
+        ipconfig /displaydns | Out-File -FilePath $ComputerDesktopPath"\Network\ipconfig-displaydns_$ComputerName.txt"
+        ipconfig /all | Out-File -FilePath $ComputerDesktopPath"\Network\ipconfig-all_$ComputerName.txt"
+        if($IsAdmin){
+            netstat -nbao | Out-File -FilePath $ComputerDesktopPath"\Network\netstat_active_connections_$ComputerName.txt"
+            }
 
 Write-Output "----- SCRIPT FINISHED -----"
 Read-Host -Prompt "Press Enter to exit"
